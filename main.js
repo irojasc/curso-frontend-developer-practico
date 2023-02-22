cc7447bfd6738704263a7fcfd92684fe9d58cdea
genesis_sntg <libreriagenesisstc@gmail.com>
2023-02-22 16:15:09 -0500

    Se agrega un div que oscurece la pantalla cada vez que se activa el menu

--- main.js
+++ main.js
@@ -5,15 +5,22 @@ const mobile_menu = document.querySelector(".mobile-menu");
 const carrito_icon = document.querySelector(".shopping-cart-icon");
 const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
 const cardsContainer = document.querySelector(".cards-container");
+const productDetailContainer = document.querySelector("#productDetail");
+const productDetailClose = document.querySelector(".product-detail-close");
+const div_darker = document.querySelector(".darker");
 
 nav_email.addEventListener('click', toggleMenu);
 hamburguer_menu.addEventListener('click', toggleMenu);
 carrito_icon.addEventListener('click', toggleMenu);
 
-function toggleMenu(event) {
 
-    if(event.target.className == "navbar-email")
+function toggleMenu(event) {
+    console.log(event.target.className);
+
+    if(event.target.className == "navbar-email"){
         menu_container.classList.toggle('inactive');
+        div_darker.classList.toggle('inactive');
+    }
 
     else if(event.target.className == "menu"){
         mobile_menu.classList.toggle('inactive');
@@ -28,7 +35,29 @@ function toggleMenu(event) {
             mobile_menu.classList.add('inactive');
             }
         }
+
+    else if(event.target.className == "img-product"){
+        if (!(shoppingCartContainer.classList.contains('inactive'))){
+            shoppingCartContainer.classList.add('inactive');
+            productDetailContainer.classList.remove('inactive');
+            }
+        else
+            productDetailContainer.classList.remove('inactive');
+        }
+    }
+
+
+
+/* function openProductDetailAside(){
+    productDetailContainer.classList.remove('inactive');
+} */
+
+
+function closeProductDetailAside(){
+    productDetailContainer.classList.add('inactive');
 }
+
+productDetailClose.addEventListener('click', closeProductDetailAside);
 
 const productList = [];
 productList.push({
@@ -50,7 +79,7 @@ productList.push({
 })
 
 
-productList.push({
+/* productList.push({
     name: 'bike3',
     price: 189,
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
@@ -66,7 +95,7 @@ productList.push({
     name: 'bike5',
     price: 19,
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-})
+}) */
 
 function renderProducts(arr){
     for(product of arr){
@@ -75,6 +104,8 @@ function renderProducts(arr){
 
         const productImg =  document.createElement('img');
         productImg.setAttribute('src', product.image);
+        productImg.classList.add('img-product')
+        productImg.addEventListener('click', toggleMenu);
 
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
@@ -99,5 +130,7 @@ function renderProducts(arr){
         cardsContainer.appendChild(productCard);
     }
 }
+
+
 
 renderProducts(productList);
